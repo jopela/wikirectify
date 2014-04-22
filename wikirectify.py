f5f9720b531db1defeca0b6d4d4f3693128ba460
Jonathan Pelletier <jonathan.pelletier1@gmail.com>
2014-04-22 11:26:22 -0400

    adding command line args

--- wikirectify.py
+++ wikirectify.py
@@ -3,8 +3,36 @@
 import argparse
 
 def main():
-    """rectify the coordinates of some POI in the mtrip database"""
-    print('hello world')
+    """
+    Rectify the coordinates of some POI in the mtrip database
+    """
+
+    parser = argparse.ArgumentParser()
+
+    host_default = 'mtrip-linker'
+    parser.add_argument(
+            '-H',
+            '--host',
+            help='hostname of the database. Default to {}'.format(host_default),
+            default = host_default
+            )
+
+    parser.add_argument(
+            '-u',
+            '--username',
+            help='username of the account used to connect to the DB.',
+            required = True
+            )
+
+    parser.add_argument(
+            '-p',
+            '--password',
+            help='password of the account used to connect tot he DB',
+            required = True
+            )
+
+
+
     return
 
 if __name__ == '__main__':
